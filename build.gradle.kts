fedd241c0d18ec398dde42792a4055283ff0935c
Callum Seabrook <callum.seabrook@prevarinite.com>
2023-10-01 17:33:15 +0100

    Update game SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.emortal.minestom:game-sdk:50fccbe")
+    implementation("dev.emortal.minestom:game-sdk:3e4a79a")
 
     implementation("dev.hollowcube:polar:1.3.1")
     implementation("dev.emortal:rayfast:a4a8041")
